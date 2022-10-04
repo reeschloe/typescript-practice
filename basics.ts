be4d8b04f3071074824b94b8b75562aa201d9656
Chloe Rees <reeschloe@yahoo.com>
2022-10-04 12:28:31 -0500

    progress through challenge 1

---
+++ basics.ts
@@ -0,0 +1,40 @@
+let userName: string = "Chloe";
+let hasLoggedIn: boolean = true;
+
+userName += " Rees";
+
+console.log(hasLoggedIn);
+
+let myNumber: number = 10;
+
+let myRegex: RegExp = /foo/;
+
+const names: string[] = userName.split(" ");
+const myValues: Array<number> = [1, 2, 3];
+
+interface Person {
+    first: string;
+    last: string;
+}
+
+const myPerson: Person = {
+    first: "Chloe",
+    last: "Rees"
+}
+
+const ids: Record<number, string> = {
+    10: "a",
+    20: "b"
+}
+ids[30] = "c";
+
+if (ids[30] === "d") {
+
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+[1,2,3].forEach(v => console.log(v));
+const out = [4,5,6].map((v) => v * 10);
---
+++ functions-test.ts
@@ -0,0 +1,5 @@
+import addNumbers, {addStrings, getName} from "./functions"
+
+console.log(addNumbers(1,2));
+console.log(addStrings("a", "b"));
+console.log(getName({first: 'Chloe', last: 'Rees'}))
